93c5c730a0177465f9222e494f44d86c171887c0
ori halevi <orihalevi98@gmail.com>
2025-06-23 03:54:35 +0300

    WIT

---
+++ ViewModels/IndicatorsUserControlViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RdpScopeToggler.ViewModels
+{
+    public class IndicatorsUserControlViewModel : BindableBase
+    {
+        private bool _isInternalOpen = true;
+        public bool IsInternalOpen
+        {
+            get => _isInternalOpen;
+            set => SetProperty(ref _isInternalOpen, value);
+        }
+
+        private bool _isExternalOpen;
+        public bool IsExternalOpen
+        {
+            get => _isExternalOpen;
+            set => SetProperty(ref _isExternalOpen, value);
+        }
+        public IndicatorsUserControlViewModel()
+        {
+
+        }
+    }
+}
---
+++ Views/IndicatorsUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RdpScopeToggler.Views
+{
+    /// <summary>
+    /// Interaction logic for IndicatorsUserControl
+    /// </summary>
+    public partial class IndicatorsUserControl : UserControl
+    {
+        public IndicatorsUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
